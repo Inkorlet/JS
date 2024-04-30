fa7cebd7c03075876b57585730378625d752e160
Inkorlet <vadimpustovit@gmail.com>
2024-04-30 11:46:50 +0300

    hw1.1

---
+++ HW1/hw1.js
@@ -0,0 +1,39 @@
+let n = "hello";
+let o = "owu";
+let p = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let j = -999;
+let h = 123;
+let i = 3.14;
+let g = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+console.log(n)
+console.log(o)
+console.log(p)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(j)
+console.log(h)
+console.log(i)
+console.log(g)
+console.log(k)
+console.log(l)
+console.log(m)
+
+let firstName = "Пустовіт";
+let middleName = "Вадим";
+let lastName = "Вячеславович";
+let person = firstName + " " + middleName +" "+ lastName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
